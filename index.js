b8cda6aacd5ef493d3a5531873879ec3b5bba693
klochkovS <fluurr@gmail.com>
2018-08-24 14:23:57 +0300

    Did 1 task

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-'use strict'
 
+function CoffeeMachine(power) {
+  this.waterAmount = 0;
+
+  const WATER_HEAT_CAPACITY = 4200;
+  let timerId;
+
+  const getBoilTime = function () {
+    return this.waterAmount + WATER_HEAT_CAPACITY * 80 / power;
+  }.bind(this);
+
+  function onReady() {
+    console.log('Coffee is ready');
+  }
+
+  this.run = function () {
+    timerId = setTimeout(onReady, getBoilTime());
+  };
+
+  this.stop = function () {
+    clearTimeout(timerId);
+  };
+}
+
+const coffeeMachine = new CoffeeMachine(100);
+coffeeMachine.waterAmount = 200;
+
+coffeeMachine.run();
+coffeeMachine.stop();
